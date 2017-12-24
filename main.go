3ffda5454f0ff2c91f712e650b6a7929abb9307d
sniperjjs <sniperjjs@163.com>
2017-12-24 17:56:04 +0800

    qry and refound
    alipay test

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
-	"go-kit/model"
+	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/relax-space/go-kit/model"
+	alipay "github.com/relax-space/lemon-alipay-sdk"
 
 	wxpay "github.com/relax-space/lemon-wxpay-sdk"
 
@@ -19,6 +22,10 @@ var (
 	certName = flag.String("CERT_NAME", os.Getenv("CERT_NAME"), "CERT_NAME")
 	certKey  = flag.String("CERT_KEY", os.Getenv("CERT_KEY"), "CERT_KEY")
 	rootCa   = flag.String("ROOT_CA", os.Getenv("ROOT_CA"), "ROOT_CA")
+
+	aliappId = flag.String("ALIPAY_APPID", os.Getenv("ALIPAY_APPID"), "ALIPAY_APPID")
+	priKey   = flag.String("ALIPAY_PRIKEY", os.Getenv("ALIPAY_PRIKEY"), "ALIPAY_PRIKEY")
+	pubKey   = flag.String("ALIPAY_PUBKEY", os.Getenv("ALIPAY_PUBKEY"), "ALIPAY_PUBKEY")
 )
 
 func main() {
@@ -44,8 +51,13 @@ func RegisterApi(e *echo.Echo) {
 	green.POST("/query", Querywx)
 	green.POST("/reverse", Reversewx)
 	green.POST("/refund", Refundwx)
+
+	green.POST("/pay2", Payal)
+	green.POST("/query2", Queryal)
+	green.POST("/refund2", Refundal)
 }
 
+/********wechat pay********/
 func Paywx(c echo.Context) error {
 
 	reqDto := wxpay.ReqPayDto{}
@@ -85,23 +97,154 @@ func Paywx(c echo.Context) error {
 	return c.JSON(http.StatusOK, SuccessResult(result))
 }
 
-func Querywx(c echo.Context) error {
-	return nil
+/************alipay***********/
+func Payal(c echo.Context) error {
+
+	reqDto := alipay.ReqPayDto{}
+	if err := c.Bind(&reqDto); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	account := Account2()
+	reqDto.ReqBaseDto = &alipay.ReqBaseDto{
+		AppId: account.AppId,
+	}
+	customDto := alipay.ReqCustomerDto{
+		PriKey: account.PriKey,
+		PubKey: account.PubKey,
+	}
+
+	result, err := alipay.Pay(&reqDto, &customDto)
+	if err != nil {
+		fmt.Printf("%+v\n", result)
+		if err.Error() == "MESSAGE_PAYING" {
+			queryDto := alipay.ReqQueryDto{
+				ReqBaseDto: reqDto.ReqBaseDto,
+				//	OutTradeNo: result["out_trade_no"].(string),
+				OutTradeNo: result.OutTradeNo,
+			}
+			result, err = alipay.LoopQuery(&queryDto, &customDto, 40, 2)
+			if err == nil {
+				return c.JSON(http.StatusOK, model.Result{Success: true, Result: result})
+			} else {
+				reverseDto := alipay.ReqReverseDto{
+					ReqBaseDto: reqDto.ReqBaseDto,
+					//		OutTradeNo: result["out_trade_no"].(string),
+					OutTradeNo: result.OutTradeNo,
+				}
+				_, err = alipay.Reverse(&reverseDto, &customDto, 10, 10)
+				return c.JSON(http.StatusInternalServerError, model.Result{Success: false, Error: model.Error{Code: 10004, Message: err.Error()}})
+			}
+		} else {
+			return c.JSON(http.StatusInternalServerError, model.Result{Success: false, Error: model.Error{Code: 10004, Message: err.Error()}})
+		}
+	}
+	fmt.Printf("xxxx\n")
+	return c.JSON(http.StatusOK, SuccessResult(result))
 }
 
+/****wxqury***/
+func Querywx(c echo.Context) error {
+	reqDto := wxpay.ReqQueryDto{}
+	if err := c.Bind(&reqDto); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	account := Account()
+	reqDto.ReqBaseDto = &wxpay.ReqBaseDto{
+		AppId: account.AppId,
+		MchId: account.MchId,
+	}
+	cusomDto := wxpay.ReqCustomerDto{
+		Key: account.Key,
+	}
+	result, err := wxpay.Query(&reqDto, &cusomDto)
+	//fmt.Printf("%+v,%v", result, err)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	return c.JSON(http.StatusOK, SuccessResult(result))
+}
+
+/**ali qurry**/
+func Queryal(c echo.Context) error {
+	reqDto := alipay.ReqQueryDto{}
+	if err := c.Bind(&reqDto); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	fmt.Printf("%+v\n", reqDto)
+	account := Account2()
+	fmt.Printf("%+v\n", account)
+	reqDto.ReqBaseDto = &alipay.ReqBaseDto{
+		AppId: account.AppId,
+	}
+	customDto := alipay.ReqCustomerDto{
+		PriKey: account.PriKey,
+		PubKey: account.PubKey,
+	}
+	result, err := alipay.Query(&reqDto, &customDto)
+	//fmt.Printf("%+v,%v", result, err)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	return c.JSON(http.StatusOK, SuccessResult(result))
+}
 func Reversewx(c echo.Context) error {
 	return nil
 }
 
+/** wxrefund**/
 func Refundwx(c echo.Context) error {
-	return nil
+	reqDto := wxpay.ReqRefundDto{}
+	if err := c.Bind(&reqDto); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	account := Account()
+	reqDto.ReqBaseDto = &wxpay.ReqBaseDto{
+		AppId: account.AppId,
+		MchId: account.MchId,
+	}
+	cusomDto := wxpay.ReqCustomerDto{
+		Key:          account.Key,
+		CertPathName: account.CertPathName,
+		CertPathKey:  account.CertPathKey,
+		RootCa:       account.RootCa,
+	}
+	result, err := wxpay.Refund(&reqDto, &cusomDto)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	return c.JSON(http.StatusOK, SuccessResult(result))
 }
 
+/**alirefund**/
+func Refundal(c echo.Context) error {
+	reqDto := alipay.ReqRefundDto{}
+	if err := c.Bind(&reqDto); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	account := Account2()
+	reqDto.ReqBaseDto = &alipay.ReqBaseDto{
+		AppId: account.AppId,
+	}
+	customDto := alipay.ReqCustomerDto{
+		PriKey: account.PriKey,
+		PubKey: account.PubKey,
+	}
+	result, err := alipay.Refund(&reqDto, &customDto)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResult(err.Error()))
+	}
+	return c.JSON(http.StatusOK, SuccessResult(result))
+}
 func Account() greenAccount {
 
 	account := greenAccount{AppId: *appId, Key: *key, MchId: *mchId,
 		CertPathName: *certName, CertPathKey: *certKey, RootCa: *rootCa,
 	}
+	return account
+}
+func Account2() greenAccount2 {
+
+	account := greenAccount2{AppId: *aliappId, PriKey: *priKey, PubKey: *pubKey}
 	return account
 }
 
@@ -112,6 +255,12 @@ type greenAccount struct {
 	CertPathName string
 	CertPathKey  string
 	RootCa       string
+}
+
+type greenAccount2 struct {
+	AppId  string
+	PriKey string
+	PubKey string
 }
 
 func ErrorResult(errMsg string) (result model.Result) {
--- main_test.go
+++ main_test.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"go-kit/model"
-	"kit/test"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/relax-space/go-kit/model"
+	"github.com/relax-space/go-kit/test"
 
 	"github.com/labstack/echo"
 )
@@ -16,8 +17,8 @@ import (
 func Test_Paywx(t *testing.T) {
 	bodyStr := `
 	{
-		"auth_code":"134625966445134769",
-		"body":"xiaoxinmiao test",
+		"auth_code":"134633484682204254",
+		"body":"likun test",
 		"total_fee":1
 	}`
 	req, err := http.NewRequest(echo.POST, "/v1/green/pay", strings.NewReader(bodyStr))
@@ -26,6 +27,96 @@ func Test_Paywx(t *testing.T) {
 	rec := httptest.NewRecorder()
 	c := echo.New().NewContext(req, rec)
 	test.Ok(t, Paywx(c))
+	v := model.Result{}
+	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
+	fmt.Printf("%+v", v)
+	test.Equals(t, http.StatusOK, rec.Code)
+}
+
+func Test_Querywx(t *testing.T) {
+	bodyStr := `
+	{
+		"out_trade_no": "1424126122414368810709856110192"
+	}`
+	req, err := http.NewRequest(echo.POST, "/v1/green/query", strings.NewReader(bodyStr))
+	test.Ok(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	test.Ok(t, Querywx(c))
+	v := model.Result{}
+	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
+	fmt.Printf("%+v", v)
+	test.Equals(t, http.StatusOK, rec.Code)
+}
+
+func Test_Refoundwx(t *testing.T) {
+	bodyStr := `
+	{
+		"out_trade_no": "1424126122414368810709856110192",
+		"refund_fee": 1
+
+	}`
+	req, err := http.NewRequest(echo.POST, "/v1/green/refund", strings.NewReader(bodyStr))
+	test.Ok(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	test.Ok(t, Refundwx(c))
+	v := model.Result{}
+	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
+	fmt.Printf("%+v", v)
+	test.Equals(t, http.StatusOK, rec.Code)
+}
+
+func Test_Payali(t *testing.T) {
+	bodyStr := `
+	{
+		"auth_code":"287175417948547958",
+		"subject":"likun test",
+		"total_amount":0.01
+	}`
+	req, err := http.NewRequest(echo.POST, "/v1/green/pay2", strings.NewReader(bodyStr))
+	test.Ok(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	test.Ok(t, Payal(c))
+	v := model.Result{}
+	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
+	fmt.Printf("%+v\n", v)
+	test.Equals(t, http.StatusOK, rec.Code)
+}
+
+func Test_Qreryali(t *testing.T) {
+	bodyStr := `
+	{
+		"out_trade_no":"111712254348458461972873388"
+	}`
+	req, err := http.NewRequest(echo.POST, "/v1/green/query2", strings.NewReader(bodyStr))
+	test.Ok(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	test.Ok(t, Queryal(c))
+	v := model.Result{}
+	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
+	fmt.Printf("%+v", v)
+	test.Equals(t, http.StatusOK, rec.Code)
+}
+
+func Test_Refundali(t *testing.T) {
+	bodyStr := `
+	{
+		"out_trade_no":"111712258546243146624551637",
+		"refund_amount":0.01
+	}`
+	req, err := http.NewRequest(echo.POST, "/v1/green/refund2", strings.NewReader(bodyStr))
+	test.Ok(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+	test.Ok(t, Refundal(c))
 	v := model.Result{}
 	test.Ok(t, json.Unmarshal(rec.Body.Bytes(), &v))
 	fmt.Printf("%+v", v)
